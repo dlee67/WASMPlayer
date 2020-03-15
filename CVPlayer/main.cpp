dbaf3a7290bc3c4c3684d1fa07208a50ff0cdf31
dlee67 <dlee67@msudenver.edu>
2020-03-14 21:03:52 -0600

    OpenCV did what Qt couldn't do for me.

---
+++ CVPlayer/main.cpp
@@ -0,0 +1,52 @@
+//!!!!!!!!!!!!
+//  Literal copy pasta from the below:
+//  https://www.learnopencv.com/read-write-and-display-a-video-using-opencv-cpp-python/
+//!!!!!!!!!!!!
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/videoio.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main(){
+
+  // Create a VideoCapture object and open the input file
+  // If the input is the web camera, pass 0 instead of the video file name
+  VideoCapture cap("../Sample.mpg"); 
+   
+  // Check if camera opened successfully
+  if(!cap.isOpened()){
+    cout << "Error opening video stream or file" << endl;
+    return -1;
+  }
+	
+  while(1){
+
+    Mat frame;
+    // Capture frame-by-frame
+    cap >> frame;
+ 
+    // If the frame is empty, break immediately
+    if (frame.empty())
+      break;
+
+    // Display the resulting frame
+    imshow( "Frame", frame );
+
+    // Press  ESC on keyboard to exit
+    char c=(char)waitKey(25);
+    if(c==27)
+      break;
+  }
+ 
+  // When everything done, release the video capture object
+  cap.release();
+
+  // Closes all the frames
+  destroyAllWindows();
+	
+  return 0;
+}
